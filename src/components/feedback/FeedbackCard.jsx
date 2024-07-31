a480cbb96d8e0ed34a9f2ccaeb2979bd79595c55
Umar Khan <56593023+umar5727@users.noreply.github.com>
2024-07-31 23:51:36 +0530

    added to animation services section

--- src/components/feedback/FeedbackCard.jsx
+++ src/components/feedback/FeedbackCard.jsx
@@ -2,23 +2,26 @@ import { faFaceSmile, faStar } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
-const FeedbackCard = ({className=''}) => {
+const FeedbackCard = ({ className = '', ...props }) => {
   return (
-    <div className={`flex flex-col gap-2  justify-center items-center px-4 py-8 bg-white dark:bg-card-dark-bg dark:text-white rounded-lg ${className}`}>        
-        <FontAwesomeIcon icon={faFaceSmile} className='w-10 h-10 rounded-full '/>
-        <p className={`my-2 text-center text-sm text-primary-text-normal dark:text-primary-text-normal-dark`}> Moonlight newspaper up its enjoyment agreeable depending. Timed voice share led him to widen noisy young. At weddings believed laughing </p>
-        <div className="flex gap-1">
-         
+    <div
+      className={`flex flex-col gap-2  justify-center items-center px-4 py-8 bg-white dark:bg-card-dark-bg dark:text-white rounded-lg ${className}`}
+      {...props}
+    >
+      <FontAwesomeIcon icon={faFaceSmile} className='w-10 h-10 rounded-full ' />
+      <p className={`my-2 text-center text-sm text-primary-text-normal dark:text-primary-text-normal-dark`}> Moonlight newspaper up its enjoyment agreeable depending. Timed voice share led him to widen noisy young. At weddings believed laughing </p>
+      <div className="flex gap-1">
 
-          <FontAwesomeIcon icon={faStar} className='text-primary-yellow text-sm'/>
-          <FontAwesomeIcon icon={faStar} className='text-primary-yellow text-sm'/>
-          <FontAwesomeIcon icon={faStar} className='text-primary-yellow text-sm'/>
-          <FontAwesomeIcon icon={faStar} className='text-primary-yellow text-sm'/>
-          <FontAwesomeIcon icon={faStar} className='text-primary-yellow text-sm'/>
-        </div>
-        
-        <h3 className='font-bold '>Umar Khan</h3>
-        
+
+        <FontAwesomeIcon icon={faStar} className='text-primary-yellow text-sm' />
+        <FontAwesomeIcon icon={faStar} className='text-primary-yellow text-sm' />
+        <FontAwesomeIcon icon={faStar} className='text-primary-yellow text-sm' />
+        <FontAwesomeIcon icon={faStar} className='text-primary-yellow text-sm' />
+        <FontAwesomeIcon icon={faStar} className='text-primary-yellow text-sm' />
+      </div>
+
+      <h3 className='font-bold '>Umar Khan</h3>
+
     </div>
   )
 }
--- src/components/feedback/FeedbackContainer.jsx
+++ src/components/feedback/FeedbackContainer.jsx
@@ -8,33 +8,48 @@ const FeedbackContainer = () => {
   return (
     <section className="mt-10 py-20 relative before:block before:absolute before:bg-primary-light before:left-2/4 before:-translate-x-2/4 before:w-screen before:h-full before:-z-10 before:content-[''] before:top-0  ">
       <div className="flex flex-col lg:flex-row ">
-      <div className="flex justify-center items-center  gap-2 flex-col my-4 order-1 text-center lg:order-2 lg:text-left lg:items-start lg:w-2/5 p-4 lg:gap-4">
-        <h2 className="text-3xl font-bold lg:text-4xl">
-          Some valuable feedback from our students
-        </h2>
-        <p className="  text-sm text-primary-text-normal dark:text-primary-text-normal-dark">
-          Supposing so be resolving breakfast am or perfectly. It drew a hill
-          from me. Valley by oh twenty direct me so. Departure defective
-          arranging rapturous did believe him all had supported. Family months
-          lasted simple set nature vulgar him. Picture for attempt joy excited
-          ten carried manners talking how.
-        </p>
-        <Button className="hover:text-white">          
-          View Reviews 
-        </Button>
-      </div>
-      {/* heading and discription ends  */}
-
-      <div className="grid gap-5 md:grid-cols-2 order-2   lg:order-1 lg:w-3/5  lg:flex lg:flex-wrap  lg:gap-10">
-        {/* <div className='flex gap-2'> */}
-        <FeedbackCard className="shadow-custom lg:relative lg:top-28 lg:w-7/12 " />
-        <FeedbackCard className=" shadow-custom lg:w-3/12 " />
-        {/* </div> */}
-        <div className="hidden lg:block lg:relative lg:w-7/12 h-fit lg:top-28 ">
-          <RatingCard className="shadow-custom first-line: lg:w-fit lg:h-fit lg:float-right" />
+        <div
+          className="flex justify-center items-center  gap-2 flex-col my-4 order-1 text-center lg:order-2 lg:text-left lg:items-start lg:w-2/5 p-4 lg:gap-4"
+          data-aos='fade-left'
+        >
+          <h2 className="text-3xl font-bold lg:text-4xl">
+            Some valuable feedback from our students
+          </h2>
+          <p className="  text-sm text-primary-text-normal dark:text-primary-text-normal-dark">
+            Supposing so be resolving breakfast am or perfectly. It drew a hill
+            from me. Valley by oh twenty direct me so. Departure defective
+            arranging rapturous did believe him all had supported. Family months
+            lasted simple set nature vulgar him. Picture for attempt joy excited
+            ten carried manners talking how.
+          </p>
+          <Button className="hover:text-white">
+            View Reviews
+          </Button>
         </div>
-        <FeedbackCard className=" shadow-custom lg:w-4/12 lg:justify-self-end" />
-      </div>
+        {/* heading and discription ends  */}
+
+        <div className="grid gap-5 md:grid-cols-2 order-2   lg:order-1 lg:w-3/5  lg:flex lg:flex-wrap  lg:gap-10">
+          {/* <div className='flex gap-2'> */}
+          <FeedbackCard
+            className="shadow-custom lg:relative lg:top-28 lg:w-7/12 "
+            data-aos='fade-right'
+          />
+          <FeedbackCard
+            className=" shadow-custom lg:w-3/12 "
+            data-aos='fade-bottom'
+          />
+          {/* </div> */}
+          <div
+            className="hidden lg:block lg:relative lg:w-7/12 h-fit lg:top-28 "
+            data-aos='fade-right'
+          >
+            <RatingCard className="shadow-custom first-line: lg:w-fit lg:h-fit lg:float-right" />
+          </div>
+          <FeedbackCard
+            className=" shadow-custom lg:w-4/12 lg:justify-self-end"
+            data-aos='fade-top'
+          />
+        </div>
       </div>
     </section>
   );
