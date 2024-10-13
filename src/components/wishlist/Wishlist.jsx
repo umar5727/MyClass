0bb968780087f5e1e5caf8f66554c9c0249cafc1
Umar Khan <56593023+umar5727@users.noreply.github.com>
2024-10-13 14:32:10 +0530

    remove comments from wishlist

--- src/components/wishlist/Wishlist.jsx
+++ src/components/wishlist/Wishlist.jsx
@@ -5,75 +5,24 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { setWishlist } from '../../app/features/authSlice';
-import useFetchData from '../../utils/fetch/useFetchData';
-import toast from 'react-hot-toast';
-import { base_url } from '../../constants/constant';
+
 import useWishlist from '../../hooks/useWishlist';
 
 
 
 const Wishlist = ({ courseId }) => {
   const { addToWishlist, removeFromWishlist } = useWishlist({ courseId: courseId });
-  // const { status, error } = useWishlist({ url: 'addWishlist', courseId: courseId })
-  // function AddToWishlist() {
-
-  //   // const { status, error } = useWishlistFetch({ url: 'addToWishlist', courseId: courseId });
-
-
-  //   if (status === 'loding') {
-  //   }
-  //   if (status === 'error') {
-  //     toast.error('Not able to Add')
-  //     console.log('wishlist error : ', error)
-  //   }
-  //   if (status === 'success') {
-  //     toast.success('Added to Wishlist')
-  //   }
-  // }
 
   const [display, setDisplay] = useState(false)
 
   const wishlistData = useSelector((state) => state.auth.wishlist)
-  const dispatch = useDispatch()
-  // async function HandleClick() {
-  //   // setStatus(state.Loading)
-  //   if (display) {
-  //     const wishlist = wishlistData?.filter((list) => {
-  //       return list != courseId
-  //     })
-  //     dispatch(setWishlist({ wishlist }))
-  //     console.log('list clicked ', wishlist)
 
-  //   } else {
 
-  //     // adding courseId to wishlist 
-
-  //     const { status, error } = useWishlistFetch({ url: 'addToWishlist', courseId: courseId });
-  //     if (status === 'loding') {
-  //     }
-  //     if (status === 'error') {
-  //       toast.error('Not able to Add')
-  //       console.log('wishlist error : ', error)
-  //     }
-  //     if (status === 'success') {
-  //       toast.success('Added to Wishlist')
-  //     }
-  //   }
-
-  //   setDisplay(!display)
-  // }
   useEffect(() => {
     setDisplay(wishlistData.includes(courseId))
 
-    // if (courseId === list) {
-
-    //   setDisplay(true)
-    // }
-    // });
-    console.log("inside useEffect ", wishlistData)
   }, [wishlistData])
-  // console.log('display wishlist : ', display)
+
 
   return (
     <div className="text-primary-danger cursor-pointer"
--- src/hooks/useWishlist.js
+++ src/hooks/useWishlist.js
@@ -1,17 +1,12 @@
-import React, { useEffect, useState } from 'react'
+
 import { setWishlist } from '../app/features/authSlice';
 import { useDispatch } from 'react-redux';
-import useFetch from './useFetch';
 import toast from 'react-hot-toast';
 import { base_url } from '../constants/constant';
 
 const useWishlist = (props) => {
-  const {  courseId } = props;
-  
-  // console.log('inside useWishlist')
-  // const [response, setResponse] = useState(null)
-  // const { status, error, response, fetchData } = useFetch()
- 
+  const { courseId } = props;
+
   const dispatch = useDispatch();
 
   const accessToken = localStorage.getItem('accessToken')
@@ -25,57 +20,49 @@ const useWishlist = (props) => {
     body: JSON.stringify({ accessToken, courseId: courseId })
   }
   const addToWishlist = async () => {
-    
-   try {
-     const res = await fetch(`${base_url}/wishlist/addToWishlist`, options)
-       
-     const response = await res.json()
-     if(!res.ok){
-       console.log('error response ; : ',response) 
-       toast.error(response.message || 'not able to add') 
-       console.log('error : ',typeof response.message)
-       throw new Error({message:response.message})
-     }
-       toast.success('added to Wishlist',response) 
-       console.log('addtowishlist : ',response.wishlist)
-       dispatch(setWishlist({ wishlist: response.wishlist }))
-       sessionStorage.setItem('wishlist', JSON.stringify(response.wishlist))
-   } catch (error) {
-      console.log('error ',error)
-   }
-        
+
+    try {
+      const res = await fetch(`${base_url}/wishlist/addToWishlist`, options)
+      const response = await res.json()
+      if (!res.ok) {
+        toast.error(response.message || 'not able to add')
+        throw new Error({ message: response.message })
       }
-      
-   
+      toast.success('added to Wishlist', response)
+
+      dispatch(setWishlist({ wishlist: response.wishlist }))
+      sessionStorage.setItem('wishlist', JSON.stringify(response.wishlist))
+    } catch (error) {
+      console.log('error ', error)
+    }
+  }
 
 
- const removeFromWishlist =async()=>{
-  // console.log('remove from wishlist called')
- try {
-   const res = await fetch(`${base_url}/wishlist/removeFromWishlist`, options)
-     const response = await res.json();
-     if(!res.ok){
- 
-       console.log('response error; : ',response) 
-       toast.error(response.message || 'not able to remove') 
-       console.log('error : ',message)
-       throw new Error({message:response.message})
-   }
-     
-   
-       toast.success(response.message ||'removed from Wishlist') 
-  
-         console.log('removed from wishlist : ',response)
-         dispatch(setWishlist({ wishlist: response.wishlist }))
-         sessionStorage.setItem('wishlist', JSON.stringify(response.wishlist))
- } catch (error) {
-  console.log('error ',error)
- }
-      
-  
- }
 
-  return {addToWishlist,removeFromWishlist}
+
+  const removeFromWishlist = async () => {
+
+    try {
+      const res = await fetch(`${base_url}/wishlist/removeFromWishlist`, options)
+      const response = await res.json();
+      if (!res.ok) {
+        toast.error(response.message || 'not able to remove')
+
+        throw new Error({ message: response.message })
+      }
+
+      toast.success(response.message || 'removed from Wishlist')
+
+      dispatch(setWishlist({ wishlist: response.wishlist }))
+      sessionStorage.setItem('wishlist', JSON.stringify(response.wishlist))
+    } catch (error) {
+      console.log('error ', error)
+    }
+
+
+  }
+
+  return { addToWishlist, removeFromWishlist }
 }
 
 export default useWishlist
